7cf0665a6cb3220dbf9a978e5e0c5b2d41991538
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2022-12-18 19:51:10 +0530

    Create solution2.cpp

---
+++ solution2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+      int i; 
+    long l; 
+    char ch; 
+    float f; 
+    double d;
+ 
+    scanf("%d %ld %c %f %lf", &i, &l, &ch, &f, &d);
+ 
+    printf("%d\n", i);
+    printf("%ld\n", l);
+    printf("%c\n", ch);
+    printf("%.3f\n", f);
+    printf("%.9lf\n", d);
+    
+    return 0;
+}
